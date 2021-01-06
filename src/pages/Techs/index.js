dce93fcd4068319263db27c784b6466391e6c693
pleonardodp12 <pauloleonardo_dp@outlook.com>
2021-01-05 22:59:39 -0300

    feat: create Tech details, and added method Get, delete and Post techs

--- src/pages/Techs/index.js
+++ src/pages/Techs/index.js
@@ -1,19 +1,55 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { ActivityIndicator, Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {
   Container,
   Form,
   Input,
+  SubmitButton,
   List,
+  Tech,
   Name,
   ProfileButton,
-  SubmitButton,
-  Tech,
 } from './styles';
 
-export default function App() {
+import api from '../../services/api';
+
+export default function Techs() {
+  const navigation = useNavigation();
+
   const [loading, setLoading] = useState(false);
   const [techs, setTechs] = useState([]);
-  const [newTech, setNewTech] = useState([]);
+  const [newTech, setNewTech] = useState(null);
+
+  async function handleAddTech() {
+    setLoading(true);
+
+    const { data } = await api.post('/techs/', {
+      id: newTech,
+    });
+
+    setTechs([...techs, data]);
+
+    setLoading(false);
+
+    setNewTech(null);
+
+    Keyboard.dismiss();
+  }
+
+  async function handleDeleteTech(id) {
+    await api.delete(`/techs/${id}`);
+
+    const filteredTechs = techs.filter((item) => item.id !== id);
+
+    setTechs(filteredTechs);
+  }
+
+  function navigationToDetail(tech) {
+    navigation.navigate('TechDetails', { tech });
+  }
 
   return (
     <Container>
@@ -24,10 +60,40 @@ export default function App() {
           placeholder="Adicionar tecnologia"
           value={newTech}
           onChangeText={setNewTech}
-          returnType="send"
-          onSubmitEditing={() => {}}
+          returnKeyType="send"
+          onSubmitEditing={handleAddTech}
         />
+        <SubmitButton loading={loading} onPress={handleAddTech}>
+          {loading ? (
+            <ActivityIndicator color="#fff" />
+          ) : (
+            <Icon name="add" size={20} color="#fff" />
+          )}
+        </SubmitButton>
       </Form>
+      <List
+        data={techs}
+        keyExtractor={(tech) => tech.id}
+        renderItem={({ item }) => (
+          <Tech>
+            <Name>{item.id}</Name>
+
+            <ProfileButton
+              background="#ffc107"
+              onPress={() => navigationToDetail(item)}
+            >
+              <Icon name="design-services" size={20} color="#fff" />
+            </ProfileButton>
+
+            <ProfileButton
+              background="#e0a800"
+              onPress={() => handleDeleteTech(item.id)}
+            >
+              <Icon name="delete" size={20} color="#fff" />
+            </ProfileButton>
+          </Tech>
+        )}
+      />
     </Container>
   );
 }
